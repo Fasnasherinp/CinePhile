676e446c580c124f9618cef2bfc1448aa0ec36ae
Fasnasherinp <fasnasherinp47@gmail.com>
2025-09-11 09:21:02 +0530

    updated

---
+++ lib/utilities/com_binding.dart
@@ -0,0 +1,21 @@
+import 'package:cinephile/utilities/api_provider.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get_storage/get_storage.dart';
+
+
+class ComBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => Api(), fenix: true);
+    Get.put(AppSession());
+    // Get.put(AppController());
+  }
+}
+
+class AppSession extends GetxController {
+  static AppSession get to => Get.find();
+  var session = GetStorage('cine_phile');
+
+}
